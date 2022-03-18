9702af687ef536521af93d08189e16dafff5472f
Welington Carlos Alves de Almeida Filho <wcfilho98@hotmail.com>
2022-03-18 11:02:52 -0300

    Ajustes no layout do perfil realizados. Próxima etapa: estilizar page de repositórios

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import { RiBookMarkLine } from "react-icons/ri";
 
 export const Container = styled.div`
   padding-left: 16px;
   padding-right: 16px;
   padding-top: 24px;
   padding-bottom: 24px;
+
+  overflow: hidden;
 `;
 
 export const Main = styled.div`
@@ -70,4 +73,74 @@ export const CalendarHeading = styled.span`
   font-size: 16px;
   margin: 36px 0 9px;
   display: inline-flex;
+`;
+
+export const RepoIcon = styled(RiBookMarkLine)`
+  width: 16px;
+  height: 16px;
+  margin-right: 4px;
+`;
+
+export const Tab = styled.div`
+  background: #fff;
+
+  .content {
+    display: flex;
+    align-items: center;
+    width: min-content;
+
+    padding: 14px 16px;
+    border-bottom: 2px solid #f9826c;
+
+    .label {
+      font-size: 14px;
+      padding: 0 7px;
+      font-weight: 600;
+    }
+    .number {
+      font-size: 12px;
+      background: rgba(209, 213, 218, 0.5);
+      padding: 2px 6px;
+      border-radius: 24px;
+    }
+  }
+  .line {
+    display: flex;
+    width: 200vw;
+    border-bottom: 1px solid #e1e4e8;
+    margin-left: -50vw;
+  }
+
+  &.mobile {
+    margin-top: 24px;
+
+    .content {
+      margin: 0 auto;
+    }
+
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
+  &.desktop {
+    display: none;
+
+    @media (min-width: 768px) {
+      display: unset;
+
+      .line {
+        margin-bottom: 24px;
+      }
+      .wrapper {
+        display: flex;
+        margin: 0 auto;
+        max-width: 1280px;
+      }
+
+      .offset {
+        width: 25%;
+        margin-right: 16px;
+      }
+    }
+  }
 `;
